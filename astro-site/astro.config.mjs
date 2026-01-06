45e252baa4793f9cf439e11f5a06bf4ff1bd6e6d
jayakeshav <jayakeshav.work@gmail.com>
2026-01-05 20:20:09 -0800

    Add sitemap integration and generate sitemap files

--- astro-site/astro.config.mjs
+++ astro-site/astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://jayakeshav.github.io",
   base: "/",
   output: "static",
+  integrations: [sitemap()],
 });
